d73c6e988d52c6079c5cd8df981dab9da3fc7d68
Nick Solonyy <nicksolony@gmail.com>
2020-02-21 02:43:40 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
+require.pry
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
@@ -13,6 +14,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
+    
     # return an array of strings containing every musician's name
     self.songs.uniq {|song| song.artist}
   end
