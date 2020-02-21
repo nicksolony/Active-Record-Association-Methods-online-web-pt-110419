2b768429c461523495f3ee4891d9da7d6a111295
Nick Solonyy <nicksolony@gmail.com>
2020-02-21 02:39:24 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,6 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.uniq {|song| song.artist}.count
   end
 
   def all_artist_names
